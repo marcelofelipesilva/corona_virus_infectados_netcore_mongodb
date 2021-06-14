17e7578ceecb4c0554c30b6499a9526fbd5657f6
marcelofelipesilva <marcelofelipesilva@outlook.com>
2021-06-14 10:15:53 -0300

    adicionado model e controller infectado.

---
+++ Controllers/InfectadoController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace corona_virus_infectados_netcore_mongodb.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class InfectadoController : ControllerBase
+    {
+        Data.MongoDB _mongoDB;
+        IMongoCollection<Infectado> _infectadosCollection;
+
+        public InfectadoController(Data.MongoDB mongoDB)
+        {
+            _mongoDB = mongoDB;
+            _infectadosCollection = _mongoDB.DB.GetCollection<Infectado>(typeof(Infectado).Name.ToLower());
+        }
+
+        [HttpPost]
+        public ActionResult SalvarInfectado([FromBody] InfectadoDto dto)
+        {
+
+            var infectado = new Infectado(dto.Id, dto.DataNascimento, dto.Sexo, dto.Estado, dto.Cidade);
+
+            _infectadosCollection.InsertOne(infectado);
+
+            return StatusCode(201, "Infectado adicionado com sucesso");
+        }
+
+        [HttpGet]
+        public ActionResult ObterInfectados()
+        {
+            var infectados = _infectadosCollection.Find(Builders<Infectado>.Filter.Empty).ToList();
+
+            return Ok(infectados);
+        }
+
+        [HttpGet("{Id}")]
+        public ActionResult ObterInfectadoPorID(string Id)
+        {
+            var infectado = _infectadosCollection.Find(
+                Builders<Infectado>.Filter.Where(_ => _.Id == Id)).ToList();
+
+            return Ok(infectado);
+        }
+        // atualizando campos atraves do id
+
+        [HttpPut]
+        public ActionResult AtualizarInfectado([FromBody] InfectadoDto dto)
+        {
+
+            var infectados = _infectadosCollection.UpdateOne(
+                Builders<Infectado>.Filter.Where(_ => _.Id == dto.Id),
+                Builders<Infectado>.Update.Set("dataNascimento", dto.DataNascimento)
+                .Set("sexo", dto.Sexo).Set("estado", dto.Estado).Set("cidade", dto.Cidade));
+
+
+
+
+            return Ok("Atualizado com sucesso");
+        }
+
+        //deleta infectado por id
+
+        [HttpDelete("{Id}")]
+        public ActionResult DeletarInfectado(string Id)
+        {
+
+            var infectados = _infectadosCollection.DeleteOne(
+                Builders<Infectado>.Filter.Where(_ => _.Id == Id));
+
+
+            return Ok("Deletado com sucesso");
+        }
+    }
+}
---
+++ Models/InfectadoDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace corona_virus_infectados_netcore_mongodb.Models
+{
+    public class InfectadoDto
+    {
+        public string Id { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string Sexo { get; set; }
+        public string Estado { get; set; }
+        public string Cidade { get; set; }
+    }
+}
